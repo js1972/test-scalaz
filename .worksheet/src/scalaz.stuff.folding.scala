e1f7df15ed8b9663c5937f7091e1cf8f3f5f4cfa
Jason Scott <jason@jaylin.com.au>
2014-09-07 11:34:50 +0800

    looking into Tags.Disjunction.subst

--- .worksheet/src/scalaz.stuff.folding.scala
+++ .worksheet/src/scalaz.stuff.folding.scala
@@ -7,16 +7,21 @@ import Tags._
 
 object folding {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(124); val res$0 = 
   List(1, 2, 3).foldRight (1) {_ * _};System.out.println("""res0: Int = """ + $show(res$0));$skip(29); val res$1 = 
-  9.some.foldLeft(2) {_ + _};System.out.println("""res1: Int = """ + $show(res$1));$skip(41); val res$2 = 
+  9.some.foldLeft(2) {_ + _};System.out.println("""res1: Int = """ + $show(res$1));$skip(43); val res$2 = 
   
   
-  List(1, 2, 3) foldMap {identity};System.out.println("""res2: Int = """ + $show(res$2));$skip(19); val res$3 = 
+  List(1, 2, 3) foldMap { identity };System.out.println("""res2: Int = """ + $show(res$2));$skip(19); val res$3 = 
   Tags.Disjunction;System.out.println("""res3: scalaz.Tag.TagOf[scalaz.Tags.Disjunction] = """ + $show(res$3));$skip(53); val res$4 = 
   Tags.Disjunction(true) |+| Tags.Disjunction(false);System.out.println("""res4: scalaz.@@[Boolean,scalaz.Tags.Disjunction] = """ + $show(res$4));$skip(59); val res$5 = 
   
   Tags.MinVal.unwrap(Tags.MinVal(1) |+| Tags.MinVal(2));System.out.println("""res5: Int = """ + $show(res$5));$skip(50); val res$6 = 
-  Tags.Disjunction.unwrap(Tags.Disjunction(true));System.out.println("""res6: Boolean = """ + $show(res$6));$skip(136); val res$7 = 
+  Tags.Disjunction.unwrap(Tags.Disjunction(true));System.out.println("""res6: Boolean = """ + $show(res$6));$skip(138); val res$7 = 
   
   //(List(1, 2, 3) foldMap {Tags.Multiplication}: Int) assert_=== 6
-  List(true, false, true, true) foldMap {Tags.Disjunction.apply};System.out.println("""res7: scalaz.@@[Boolean,scalaz.Tags.Disjunction] = """ + $show(res$7))}
+  List(true, false, true, true) foldMap { Tags.Disjunction.apply };System.out.println("""res7: scalaz.@@[Boolean,scalaz.Tags.Disjunction] = """ + $show(res$7));$skip(67); 
+  
+  val l = Tags.Disjunction.subst(List(true, false, true, true));System.out.println("""l  : List[scalaz.@@[Boolean,scalaz.Tags.Disjunction]] = """ + $show(l ))}
+  //l foldMap { Tags.Disjunction }
+  
+  
 }
--- src/main/scala/scalaz/stuff/folding.sc
+++ src/main/scala/scalaz/stuff/folding.sc
@@ -10,9 +10,8 @@ object folding {
   9.some.foldLeft(2) {_ + _}                      //> res1: Int = 11
   
   
-  List(1, 2, 3) foldMap {identity}                //> res2: Int = 6
-  Tags.Disjunction                                //> res3: scalaz.Tag.TagOf[scalaz.Tags.Disjunction] = scalaz.Tag$TagOf@1488968a
-                                                  //| 
+  List(1, 2, 3) foldMap { identity }              //> res2: Int = 6
+  Tags.Disjunction                                //> res3: scalaz.Tag.TagOf[scalaz.Tags.Disjunction] = scalaz.Tag$TagOf@313179e3
   Tags.Disjunction(true) |+| Tags.Disjunction(false)
                                                   //> res4: scalaz.@@[Boolean,scalaz.Tags.Disjunction] = true
   
@@ -21,6 +20,13 @@ object folding {
   Tags.Disjunction.unwrap(Tags.Disjunction(true)) //> res6: Boolean = true
   
   //(List(1, 2, 3) foldMap {Tags.Multiplication}: Int) assert_=== 6
-  List(true, false, true, true) foldMap {Tags.Disjunction.apply}
+  List(true, false, true, true) foldMap { Tags.Disjunction.apply }
                                                   //> res7: scalaz.@@[Boolean,scalaz.Tags.Disjunction] = true
+  
+  val l = Tags.Disjunction.subst(List(true, false, true, true))
+                                                  //> l  : List[scalaz.@@[Boolean,scalaz.Tags.Disjunction]] = List(true, false, tr
+                                                  //| ue, true)
+  //l foldMap { Tags.Disjunction }
+  
+  
 }

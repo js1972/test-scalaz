7a6d69d03fe0ad8d07bc1bf62ffa9c0c3fb2be8d
Jason Scott <jason@jaylin.com.au>
2014-09-17 19:36:29 +0800

    testing

--- src/main/scala/shapeless/stuff/type_testing.scala
+++ src/main/scala/shapeless/stuff/type_testing.scala
@@ -30,9 +30,9 @@ object TypeTesting extends App {
   // Suppose we want to write a type-level version instead:
 
   trait HasChecksum[L <: HList, S <: Nat]
-  
+
   implicit object hnilHasChecksum extends HasChecksum[HNil, _0]
-  
+
   implicit def hlistHasChecksum[
     H <: Nat, T <: HList, S <: Nat,
     TS <: Nat, TL <: Nat,
@@ -47,9 +47,10 @@ object TypeTesting extends App {
   ) = new HasChecksum[H :: T, S] {}
 
   def isValid[L <: HList](implicit l: Length.Aux[L, _9], c: HasChecksum[L, _0]) = ()
-  
+
+  // DO NOT UN-COMMENT - CRASHES ECLIPSE !!
   //println( isValid[_3 :: _4 :: _5 :: _8 :: _8 :: _2 :: _8 :: _6 :: _5 :: HNil] )
   //isValid[_3 :: _1 :: _5 :: _8 :: _8 :: _2 :: _8 :: _6 :: _5 :: HNil]
-  
+
   println("\n")
 }
